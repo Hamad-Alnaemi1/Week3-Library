d0b36a2f1c3832d336d1caf580ba9531cc1f9906
Hamad-AlNaemi1 <alnaemi.hamad@hotmail.com>
2025-10-20 17:57:51 +0300

    Updated code with variables and properties

--- Week-3-Practicle/Books.cs
+++ Week-3-Practicle/Books.cs
@@ -2,15 +2,32 @@
 
 class Book
 {
-    string Title;
-    string Author;
-    string ISBN;
-
+    private string title;
+    private string author;
+    private string isbn;
+    
+    // Title property to allow access
+    // to the title private field
+    public string Title
+    {
+        get { return title; }  // get method
+        set { title = value; } // set method
+    }
+    public string Author
+    {
+        get { return author; }
+        set { author = value; }
+    }
+    public string ISBN
+    {
+        get { return isbn; }
+        set { isbn = value; }
+    }
     public Book(string bookTitle, string bookAuthor, string bookISBN)
     {
-        Title = bookTitle;
-        Author = bookAuthor;
-        ISBN = bookISBN;
+        this.Title = bookTitle;
+        this.Author = bookAuthor;
+        this.ISBN = bookISBN;
     }
 
     public void DisplayInfo()
